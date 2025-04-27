55989e7168a3d3c0cf3497a619858c15b8dd0a3d
Giuseppe Costanzi <giuseppecostanzi@gmail.com>
2025-04-27 22:15:10 +0200

    Add files via upload

--- launcher.py
+++ launcher.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # -----------------------------------------------------------------------------
-# project:  biovarase
-# authors:  1966bc
-# mailto:   [giuseppecostanzi@gmail.com]
-# modify:   hiems MMXXIII
+# project:  biovarase
+# authors:  1966bc
+# mailto:   [giuseppecostanzi@gmail.com]
+# modify:   ver MMXXV
 # -----------------------------------------------------------------------------
 
 import os
@@ -12,35 +12,28 @@ import inspect
 import subprocess
 from threading import Thread
 
-
 class Launcher:
     def __str__(self):
         return "class: {0}\nMRO: {1}".format(self.__class__.__name__,
-                                             [x.__name__ for x in Launcher.__mro__])
+                                           [x.__name__ for x in Launcher.__mro__])
 
     def launch(self, path):
-        # TO FIX Exception in thread Thread-n: n = 1,2,3,4,...
-        #Thread(target=self.open_file(path)).start()
-        thread = Thread(target=self.open_file(path), )
+        thread = Thread(target=self._open_file, args=(path,))
         thread.start()
 
-
-    def open_file(self, path):
-
+    def _open_file(self, path):
         try:
-            do_i_exist = os.path.exists(path)
-
-            if do_i_exist:
+            if os.path.exists(path):
                 if os.name == 'posix':
                     subprocess.call(["xdg-open", path])
-                else:
+                elif os.name == 'nt':  # 'nt' stay for Windows
                     os.startfile(path)
-
+            else:
+                self.on_log(inspect.stack()[0][3], f"File non trovato: {path}", FileNotFoundError, sys.modules[__name__])
         except:
             self.on_log(inspect.stack()[0][3], sys.exc_info()[1], sys.exc_info()[0], sys.modules[__name__])
 
 def main():
-
     foo = Launcher()
     print(foo)
     input('end')
